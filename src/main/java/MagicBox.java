f2de5baf611085dfea3b970d4204e5889b0c75cb
Gromova0423 <gromova0423@gmail.com>
2023-06-22 14:35:46 +0300

    Seven commit test

--- src/main/java/MagicBox.java
+++ src/main/java/MagicBox.java
@@ -1,11 +1,12 @@
 import java.util.Random;
 
 public class MagicBox<T> {
-    public static int size;
+    public int size;
     protected T[] items;
     protected static int[] itemsTest;
     private Random random;
     private int randomInt;
+    private static int randomInt2;
 
     public MagicBox(int size) {
         this.size = size;
@@ -13,6 +14,7 @@ public class MagicBox<T> {
         this.random = new Random();
         this.randomInt = random.nextInt(size);
     }
+
 
     public boolean add(T item) {
         for (int i = 0; i < items.length; i++) {
@@ -46,6 +48,25 @@ public class MagicBox<T> {
         //System.out.println("Осталось заполнить " + sum + " элемента");
         if (sum == 0){
             itemPick = (T) items[randomInt];
+            System.out.println("Коробка заполнена. Случайный элемент: " + itemPick);
+        }
+        else {
+            throw new RuntimeException("Коробка не полна, осталось заполнить: " + sum + " ячеек");
+        }
+        return itemPick;
+    }
+
+    public static int pickTest(){
+        int sum = 0;
+        int itemPick;
+        for (int i = 0; i < itemsTest.length; i++) {
+            if (itemsTest[i] == 0) {
+                sum = sum + 1;
+            }
+        }
+        //System.out.println("Осталось заполнить " + sum + " элемента");
+        if (sum == 0){
+            itemPick = itemsTest[randomInt2];
             System.out.println("Коробка заполнена. Случайный элемент: " + itemPick);
         }
         else {
--- src/test/java/MagicBoxTest.java
+++ src/test/java/MagicBoxTest.java
@@ -2,13 +2,29 @@ import org.junit.jupiter.api.*;
 
 class MagicBoxTest {
 
+
     @Test
     void addTest_Test() {
         MagicBox.itemsTest = new int[]{5};
         int a = 4;
         boolean result = MagicBox.addTest(a);
-        Assertions.assertEquals(false, result);
+        Assertions.assertFalse(result);
     }
+
+    @Test
+    void addTest_Test2() {
+        MagicBox.itemsTest = new int[2];
+        boolean result = MagicBox.addTest(0);
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    void pickTest_Test() {
+        MagicBox.itemsTest = new int[]{1};
+        int value = MagicBox.pickTest();
+        Assertions.assertNotNull(value); //Проверить, что value не является null
+    }
+
 
     @BeforeEach
     void BeforeEach() {
